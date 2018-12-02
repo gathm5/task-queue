122d8eea935a63538c9227b347f3d9d20cc8454d
Gautham Stalin <gauthamstalin@gmail.com>
2018-12-02 02:46:57 -0800

    allow pause and logging queue operations

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 class TaskQueue {
   constructor(threshold) {
     this.QUEUE = [];
-    this.isExecuting = false;
+    this.isPaused = false;
     this.QUEUE_OVERFLOW = [];
     this.THRESHOLD_POINT = +threshold;
+    return this;
+  }
+  _log(...args) {
+    if(this.isLogsEnabled) {
+      console.log(...args);
+    }
+  }
+  enableLogs() {
+    this.isLogsEnabled = true;
+  }
+  disableLogs() {
+    this.isLogsEnabled = false;
   }
   insert(fn) {
     if (typeof fn === 'function') {
       if (this.THRESHOLD_POINT && this.QUEUE.length === this.THRESHOLD_POINT) {
+        this._log('TQ - OVERLOAD THRESHOLD', this.THRESHOLD_POINT, 'ADDING TO LINE UP', Date.now());
         this.QUEUE_OVERFLOW.push(fn);
         this._run();
         return;
       }
+      this._log('TQ - INSERT INTO MAIN QUEUE', Date.now())
       this.QUEUE.push(fn);
-      this._run();
     }
   }
   add(fn) {
     this.insert(fn);
   }
-  _run() {
-    if (!this.isExecuting) {
+  pause() {
+    this.isPaused = true;
+  }
+  process() {
+    this._run(true);
+  }
+  _run(flag) {
+    if (flag) {
+      this.isPaused = false;
+    }
+    if (!this.isPaused) {
       this._execute();
     }
   }
@@ -30,10 +52,13 @@ class TaskQueue {
       popped(() => {
         const fill = this.QUEUE_OVERFLOW.shift();
         if (fill) {
+          this._log('TQ - MOVE FROM LINE UP TO MAIN QUEUE', Date.now());
           this.QUEUE.push(fill);
         }
+        this._run();
       });
-      this._run();
+    } else {
+      this._log('TQ - QUEUE IS CLEAR NOW', Date.now());
     }
   }
 }
